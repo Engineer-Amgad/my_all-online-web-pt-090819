a616111daba0cc837a1047e3ad6682c3b4d4714f
Amgad Malik <amgadmalik@yahoo.com>
2019-09-21 15:02:59 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0 
+  block_retrun_values = []
+  while i < collection.length 
+    block_retrun_values << yield(collection[i])
+    i += 1 
+  end 
+  
+  if block_retrun_values.include?(false)
+    return false 
+  end 
 end
